1290c4853f546afcad110916c42c00ea72a7d526
Mark May <mmay618@gmail.com>
2020-06-12 19:23:13 -1000

    Hader and Footer

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import App from './App';
 
 ReactDOM.render(
@@ -18,3 +20,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
