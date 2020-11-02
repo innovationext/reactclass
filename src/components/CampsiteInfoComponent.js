fd888e54f95628b0c667cd23f4148a1301a04cff
Mark May <mmay618@gmail.com>
2020-11-01 17:03:05 -1000

    Week 4 Workshop Task 2 test

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem,
-  Button
-  } from 'reactstrap';
+  Button,
+  Modal, ModalHeader, ModalBody,
+  Form, FormGroup, Input, Label} from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 
@@ -48,14 +49,47 @@ function RenderComments({ comments }) {
 }
 
 class CommentForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state =  {
+                    isModalOpen: false
+                  };
+  }
   render() {
     return (
       <React.Fragment>
          <button outline color="secondary" className="fa-lg" outline="true"><i class="fa fa-pencil"></i> Submit Comment </button>
+
+
+         <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username"
+                                    innerRef={input => this.username = input } />
+                            </FormGroup>
+                            <FormGroup>
+                            <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password" 
+                                    innerRef={input => this.password = input} />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember" 
+                                        innerRef={input => this.remember = input} />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
       </React.Fragment>
     );
   };
-}
+}   
 
 
 function CampsiteInfo(props) {
